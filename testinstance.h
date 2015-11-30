90a46f55e8273a8a57da5246f1c91e5932c44429
James Byron <jameslbyron@gmail.com>
2015-11-29 19:53:44 -0800

    Re-added ti files

---
+++ testinstance.h
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include "config.h"
+#include "individual2.h"
+
+using namespace std;
+
+class TestInstance
+{
+public:
+  TestInstance() {};
+  TestInstance(string str);
+  ~TestInstance();
+  string humanReadable() { return datasetForm; };
+  unsigned char * getBinary() { return binary; };
+  int getDepth() const { return depth; };
+  string getStringRep();
+  int classify(Individual2* individual);
+  float fitnessHiFi(Individual2* individual);
+private:
+  int charDifference(char c, char d) { return (c-d)+1; };
+  string intToBinary(int i);
+  string byteToString(unsigned char c);
+  int distToNearCorner(int bkr, int bkf);
+  int distToNearEdge(int rnk, int fl);
+  int lowbyte(int i);
+  int hibyte(int i);
+  void countFeats(signed char * featcounts, Individual2 * ind);
+  unsigned char binary[NUM_FEATURES];
+  string datasetForm;
+  int whiteKingFile;
+  int whiteKingRank;
+  int whiteRookFile;
+  int whiteRookRank;
+  int blackKingFile;
+  int blackKingRank;
+  int depth;
+};
