78dea4360c8d1a916cc321901664366435baa078
James Byron <jamesbyron84@gmail.com>
2016-04-24 22:31:22 -0700

    revert to more debug

--- SingleNode.cpp
+++ SingleNode.cpp
@@ -283,14 +283,14 @@ vector<float> getDiversityValues(vector<float>* values, int numZeros) {
 	output[3] = var / (float)counter; // counter = POP_SIZE * NUM_ISLANDS
 	cout << output[3] << endl;
 	output[4] = numZeros;
-	/*if (rand() < RAND_MAX/100) {
+	if (rand() < RAND_MAX/100) {
 			cout << endl;
 			for (int i = 0; i < (*values).size(); ++i) {
 				cout << (*values)[i] << ",";
 			}
 			cout << endl;
 			exit(0);
-		}*/
+		}
 	return output;
 }
 
